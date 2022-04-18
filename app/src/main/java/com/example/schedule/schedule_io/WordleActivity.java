5500fa123d43aee56b46052d7fcf362f78d314bc
Emeric <emeric.dissez@gmail.com>
2022-04-18 17:05:47 +0100

    Wordle finished finally

--- app/src/main/java/com/example/schedule/schedule_io/WordleActivity.java
+++ app/src/main/java/com/example/schedule/schedule_io/WordleActivity.java
@@ -10,6 +10,10 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -17,11 +21,15 @@ import java.io.InputStreamReader;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 
 public class WordleActivity extends AppCompatActivity {
 
-    String[] words;
+    FirebaseAuth mAuth;
+
+    String[] words = new String[5757];
     String solution = "";
     int stage = 0;
     int[] ids = {
@@ -92,9 +100,7 @@ public class WordleActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         this.supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.wordle);
-
-        words = new String[5757];
-
+        mAuth = FirebaseAuth.getInstance();
         TextView time = (TextView)findViewById(R.id.textView27);
         timer = new CountDownTimer(300000, 1000) {
 
@@ -105,6 +111,8 @@ public class WordleActivity extends AppCompatActivity {
             public void onFinish() {
                 this.cancel();
                 finalscore();
+                updateScore(score);
+
             }
         };
         timer.start();
@@ -231,12 +239,10 @@ public class WordleActivity extends AppCompatActivity {
         }
     }
 
-    //pass word
     public void pass(View view){
         reset();
     }
 
-    //reset layout and change solution word
     public void reset(){
         double rnd = Math.random() * 5757;
         solution = words[(int) rnd];
@@ -261,7 +267,6 @@ public class WordleActivity extends AppCompatActivity {
         key.setBackground(getDrawable(R.drawable.keyboard));
     }
 
-    //times each letter is in solution word
     public int[] In(){
         int[] times = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
         for (int a = 0; a<5; a++){
@@ -293,5 +298,13 @@ public class WordleActivity extends AppCompatActivity {
             }
         }
         return rights;
+    }
+
+    public void updateScore(int score){
+        DatabaseReference rootref = FirebaseDatabase.getInstance("https://leword-549be-default-rtdb.europe-west1.firebasedatabase.app/").getReference();
+        Map<String,Object> map = new HashMap<>();
+        map.put("score",score);
+        rootref.child("Users").child(mAuth.getUid()).updateChildren(map);
+
     }
 }
