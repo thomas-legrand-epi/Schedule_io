1894448a85cc6c02994f28d9cbd73f8b5c2873e3
Thomas Legrand <76771848+thomas-legrand-epi@users.noreply.github.com>
2022-04-19 11:10:52 +0100

    FIn du Leaderboard

--- app/src/main/java/com/example/schedule/schedule_io/LeaderboardActivity.java
+++ app/src/main/java/com/example/schedule/schedule_io/LeaderboardActivity.java
@@ -1,14 +1,67 @@
 package com.example.schedule.schedule_io;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class LeaderboardActivity extends AppCompatActivity {
+
+    FirebaseAuth mAuth;
+    private FirebaseDatabase mDataBase;
+    private DatabaseReference mReferenceUser;
+    private List<User> users = new ArrayList<>();
+
+    public interface DataStatus{
+        void DataIsLoaded(List<User> users, List<String> keys);
+        void DataIsInserted();
+        void DataIsUpdated();
+        void DataIsDeleted();
+
+
+
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.leaderboard);
+
+        mDataBase = FirebaseDatabase.getInstance("https://leword-549be-default-rtdb.europe-west1.firebasedatabase.app/");
+        mReferenceUser = mDataBase.getReference("Users");
+
+    }
+    public void readData(final DataStatus dataStatus){
+        mReferenceUser.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                users.clear();
+                List<String> keys = new ArrayList<>();
+                for(DataSnapshot keyNode : snapshot.getChildren()){
+                    keys.add(keyNode.getKey());
+                    User user = keyNode.getValue(User.class);
+                    users.add(user);
+                }
+                dataStatus.DataIsLoaded(users,keys);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
     }
 }
